5d5e9ffe291e90ad6a88b1904e9cbf1550ccabd5
Bruno Nicholas <sbnibro256@gmail.com>
2019-11-12 17:22:21 +0300

    refactoring error of menu disappearing

--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -30,26 +30,31 @@
             top: 0;
             width: 100%;
         }
+        .body-stuff {
+            position: absolute;
+            top: 50px;
+        }
     </style>
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
     <div class="wrapper">
         @include('layouts.includes.header')
         <!-- Left side column. contains the logo and sidebar -->
-        @include('layouts.includes.side')
-
-        <!-- Content Wrapper. Contains page content -->
-        <div class="content-wrapper">
-            <section class="content-header">
-                @yield('page-tree')
-            </section>
-            @include('layouts.includes.notifications')
-            <section class="content">
-                @yield('content')
-            </section>
+        <div style="width: 100%; margin-top: 50px;">
+            @include('layouts.includes.side')
+            <!-- Content Wrapper. Contains page content -->
+            <div class="content-wrapper" style="">
+                <section class="content-header">
+                    @yield('page-tree')
+                </section>
+                @include('layouts.includes.notifications')
+                <section class="content">
+                    @yield('content')
+                </section>
+            </div>
+            <!-- /.content-wrapper -->
+            @include('layouts.includes.footer')
         </div>
-        <!-- /.content-wrapper -->
-        @include('layouts.includes.footer')
 
         <!-- Control Sidebar -->
         @include('layouts.includes.rightmenu')
@@ -73,7 +78,7 @@
     <script src="{{ asset('assets/js/pages/dashboard2.js') }}"></script>
     <script src="{{ asset('assets/js/demo.js') }}"></script>
     @yield('scripts')
-    <script type="text/javascript">
+    <script>
         function showTime(){
             var date = new Date();
             var h = date.getHours();    // 0 - 23
@@ -104,9 +109,11 @@
         showTime();
     </script>
     <script>
+        window.onload = function() {myFunction()};
         window.onscroll = function() {myFunction()};
 
         var navbar = document.getElementById("navbar");
+        var content_header
         var sticky = navbar.offsetTop;
 
         function myFunction() {
