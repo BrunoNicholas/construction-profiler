3cc0901bca1b4219f806f35cc3d65cbfcaec202d
Bruno Nicholas <sbnibro256@gmail.com>
2019-11-21 12:06:36 +0300

    adding a company

--- resources/views/system/companies/create.blade.php
+++ resources/views/system/companies/create.blade.php
@@ -23,7 +23,91 @@
                         <div class="card">
                             <div class="card-header"><h4 style="width: 100%; text-align: center;"> Add Company </h4></div>
                             <div class="card-body">
+                                <form action="{{ route('companies.store') }}" method="POST">
+                                    @csrf
 
+                                    @foreach ($errors->all() as $error)
+                                        <p class="alert alert-danger">{{ $error }}</p>
+                                    @endforeach
+
+                                    @if (session('success'))
+                                        <div class="alert alert-success">
+                                            {{ session('success') }}
+                                        </div>
+                                    @endif
+                                            
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+                                    <!-- Step 1 -->
+                                    <br>
+                                    <section class="row">
+                                        <div class="col-md-12">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="file_item">Company Name :</label>
+                                                        <input type="text" name="company_name" class="form-control" id="file_item" placeholder="Your Profile Name">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="file_item">Company Email :</label>
+                                                        <input type="email" name="company_email" class="form-control" id="file_item" placeholder="Your Profile Email">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="file_item">Company Telephone :</label>
+                                                        <input type="text" name="company_telephone" class="form-control" id="file_item" placeholder="Your Profile Email">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="file_item">Location :</label>
+                                                        <input type="text" name="company_location" class="form-control" id="file_item" placeholder="GPS Cordinates Location (E.g 1.243 32.5511)">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="image">Company Logo :</label>
+                                                        <input type="file" id="image" class="form-control" name="company_logo" accept=".jpg, .png, .jpeg">
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label>Company Status : </label>
+                                                        <div class="">
+                                                            <input type="radio" name="status" id="caption_text" placeholder="Caption Text" value="Active"> <label for="caption_text"> Active </label> 
+                                                            <input type="radio" name="status" id="caption_text" placeholder="Caption Text" value="Busy"> <label for="caption_text"> Busy </label> 
+                                                            <input type="radio" name="status" id="caption_text1" placeholder="Caption Text" value="Pending" checked> <label for="caption_text1"> Pending </label> 
+                                                            <input type="radio" name="status" id="caption_text2" placeholder="Caption Text" value="Achived"> <label for="caption_text2"> Achived </label>
+                                                        </div>
+                                                    </div>
+                                                    <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="col-md-12">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="desc_prof"> Profile Description : </label>
+                                                        <textarea name="company_description" rows="8" class="form-control" id="desc_prof" placeholder="Your company description for your profile."></textarea>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <label for="desc_prof"> Profile Bio : </label>
+                                                        <textarea name="company_bio" rows="8" class="form-control" id="desc_prof" placeholder="Your company bio information."></textarea>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </section>
+                                    <div div class="col-md-12 text-center">
+                                        <a href="{{ route('profiles.index') }}" class="btn btn-rounded btn-info" style="min-width: 150px;">Back</a>
+                                        <button type="submit" class="btn btn-rounded btn-primary" style="min-width: 150px;"> Create Profile</button>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                     </div>
--- resources/views/system/companies/index.blade.php
+++ resources/views/system/companies/index.blade.php
@@ -23,8 +23,65 @@
 				<div class="tab-content padding">
 					<div class="chart tab-pane active" id="urevenue-chart" style="position: relative; height: 500px; overflow-y: auto;">
 						<div class="card">
-
-							
+              <div class="card-header"><h4 style="width: 100%; text-align: center;"> Current Companies </h4></div>
+              <div class="card-body">
+                  <div class="row">
+                      @if(sizeof($companies) < 1)
+                        <div class="col-12" style="padding-left: 10px; border-radius: 5px;">
+                          <p class="alert alert-danger">No company found!</p>
+                        </div>
+                      @endif
+                      
+                      @foreach($companies as $company)
+                          <div class="col-md-6 pt-2">
+                              <div class="row" onclick="window.location='{{ route('companies.show', $company->id) }}'" title="Click to view company details">
+                                  <div class="col-md-6">
+                                      <a>
+                                          <div class="panel  panel-hover text-center">
+                                              @if($company->description_image)
+                                                  <img class="panel-img-top img-responsive" src="{{ asset('files/companies/images/' . $company->description_image) }}" alt="Card image cap" style="height: 210px; width: auto; border-radius: 5px;">
+                                              @else
+                                                  <img class="panel-img-top img-responsive" src="{{ asset('img/favicon.png') }}" alt="Card image cap" style="height: 200px; width: auto;">
+                                              @endif
+                                              <div class="panel-body">
+                                                  <div class="d-flex no-block align-items-center m-b-15">
+                                                      <span class="pull-left" title="Start date"><i class="fa fa-calendar"></i> {{ $company->start_date }} </span>
+                                                      <div class="ml-auto pull-right" title="End date">
+                                                          <span><i class="fa fa-calendar"></i> {{ $company->end_date }} </span>
+                                                      </div>
+                                                  </div>
+                                              </div>
+                                          </div>
+                                      </a>
+                                  </div>
+                                  <div class="col-md-6">
+                                      <table class="table table-bordered table-striped">
+                                          <tbody>
+                                              <tr>
+                                                  <td><h5 class="font-normal text-center"> {{ $company->name }} </h5></td>
+                                              </tr>
+                                              <tr>
+                                                  <td><a href="{{ $company->department_id ? route('departments.show', $company->department_id) : '' }}" target="_blank"><i class="fa fa-tree"></i> {{ App\Models\Department::where('id',$company->department_id)->first()->department_name }}</a></td>
+                                              </tr>
+                                              <tr>
+                                                  <td>{{ $company->description }}</td>
+                                              </tr>
+                                              <tr>
+                                                  <td style="">{{ $company->status }}</td>
+                                              </tr>
+                                              @if(sizeof($company->posts) > 0)
+                                              <tr>
+                                                  <td>{{ $company->posts->count() }} Posts</td>
+                                              </tr>
+                                              @endif
+                                          </tbody>
+                                      </table>
+                                  </div>
+                              </div>
+                          </div>
+                      @endforeach
+                  </div>
+              </div>							
 						</div>
 					</div>
 				</div>
